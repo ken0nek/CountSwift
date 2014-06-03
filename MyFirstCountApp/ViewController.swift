adb2847c3adc45d146397576b671490df263b8eb
Ken Tominaga <ken.for.develop@gmail.com>
2014-06-04 01:03:10 +0900

    Initial Commit

---
+++ MyFirstCountApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstCountApp
+//
+//  Created by Ken Tominaga on 2014/06/04.
+//  Copyright (c) 2014年 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
