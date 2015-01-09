59a759163e8ceec7d98107cfd25d010ecbf4beb4
Ken Tominaga <ken.for.develop@gmail.com>
2015-01-09 17:42:59 +0900

    First commit

--- MyFirstCountApp/ViewController.swift
+++ MyFirstCountApp/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var numberLabel : UILabel
+    var number: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func operatorButtonPushed(button : UIButton) {
+        switch(button.tag) {
+        case 0:
+            number++
+            break
+        case 1:
+            number--
+            break
+        case 2:
+            number *= 2
+            break
+        case 3:
+            number /= 2
+            break
+        default:
+            break;
+        }
+        numberLabel.text = "\(number)"
+    }
 
 }
-
